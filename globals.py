76c02c1df9dec5ec1ff5fd74060e9cf47e705d86
Ricardo Yaben <32867697+RicYaben@users.noreply.github.com>
2020-02-22 17:36:50 +0100

    Update globals.py

--- globals.py
+++ globals.py
@@ -1,22 +1,25 @@
-# DICTIONARY
-# ==========
-# NYI = Not Yet Implemented
-# WIP = Work In Progress
+#### DICTIONARY
+## ==========
+## NYI = Not Yet Implemented
+## WIP = Work In Progress
 
-# COLORS
-# ======
-# Bluesky: "#88e1f2"    | Blackish: "#3a3535"   | Deep blue: "#21243d" #
-# Yellow: "#ffd082"     | Pink: "#ff7c7c"       | Grey: "#888"         #
+#### COLORS
+## ======
+## Bluesky: "#88e1f2"    | Blackish: "#3a3535"   | Deep blue: "#21243d" #
+## Yellow: "#ffd082"     | Pink: "#ff7c7c"       | Grey: "#888"         #
 
 BACKGROUND_COLOR = "#21243d"
 VISITED_COLOR = "#ffd082"
+GOAL_COLOR = "#ff7c7c"
 IDLE_COLOR = "#3a3535"
 LINE_COLOR = "#888"
 
-# VERTICES AND PROBABILITY OF HAVING AND EDGE
+#### VERTICES AND PROBABILITY OF HAVING AND EDGE
 VERTICES = 100
 PROBABILITY_OF_EDGE = 0.125
+#GOAL = 5
+#START = 18
 
-# ALGORITHM TO USE
-# BFS | DFS | Dijkstra (NYI) | A* (NYI)
-ALGORITHM = "BFS"
+#### ALGORITHM TO USE
+## BFS | DFS | dijkstra  | A* (NYI)
+#ALGORITHM = "dijkstra"
